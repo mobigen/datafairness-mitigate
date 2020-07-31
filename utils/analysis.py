a8a642897cd2c95568ef6e5c5fb00c1aa9884e1b
seungwubaek <whiteseungwu@gmail.com>
2020-07-31 16:14:44 +0900

    모델 성능 측정 지표 추가

--- utils/analysis.py
+++ utils/analysis.py
@@ -10,12 +10,19 @@ class ClassificationMetric:
         self.label_name = label_name
 
         self.origin_pos_mask = (self.orig_df[label_name] == True)
+        self.pred_pos_mask = (self.pred_df[label_name] == True)
 
     def num_positive(self):
         return np.sum(self.origin_pos_mask)
 
     def num_negative(self):
         return np.sum(~self.origin_pos_mask)
+
+    def num_pred_positive(self):
+        return np.sum(self.pred_pos_mask)
+
+    def num_pred_negative(self):
+        return np.sum(~self.pred_pos_mask)
 
     def num_true_positive(self):
         return np.sum(
@@ -45,6 +52,7 @@ class ClassificationMetric:
         FP = self.num_false_positive()
         FN = self.num_false_negative()
         return dict(TPR=TP/P, TNR=TN/N, FPR=FP/N, FNR=FN/P,
+                    PPV=TP/(TP+FP), NPV=TN/(TN+FN), FDR=FP/(TP+FP), FOR=FN/(TN+FN),
                     ACC=(TP+TN)/(P+N) if P+N > 0. else np.float64(0.))
 
     def accuracy(self):
@@ -53,3 +61,42 @@ class ClassificationMetric:
         TP = self.num_true_positive()
         TN = self.num_true_negative()
         return (TP+TN)/(P+N)
+
+    def true_positive_rate(self):
+        return self.num_positive()/self.num_true_positive()
+
+    def true_negative_rate(self):
+        return self.num_negative()/self.num_true_negative()
+
+    def false_positive_rate(self):
+        return self.num_false_positive()/self.num_negative()
+
+    def false_negative_rate(self):
+        return self.num_false_negative()/self.num_positive()
+
+    def positive_predictive_value(self):
+        return self.num_true_positive()/self.num_pred_positive()
+
+    def negative_predictive_value(self):
+        return self.num_true_negative()/self.num_pred_negative()
+
+    def false_discovery_rate(self):
+        return self.num_false_positive()/self.num_pred_positive()
+
+    def false_omission_rate(self):
+        return self.num_false_negative()/self.num_pred_negative()
+
+    def sensitivity(self):
+        return self.true_positive_rate()
+
+    def recall(self):
+        return self.true_positive_rate()
+
+    def specificity(self):
+        return self.true_negative_rate()
+
+    def fall_out(self):
+        return self.false_positive_rate()
+
+    def miss_rate(self):
+        return self.false_negative_rate()
